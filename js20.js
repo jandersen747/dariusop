c75bce84985f7d407e6849ffcd508762d024be50
jandersen747 <70488756+jandersen747@users.noreply.github.com>
2020-08-31 19:34:40 +0200

    Add files via upload

---
+++ js20.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+function isLeapYear() { 
+var year = document.getElementById("year").value;  
+document.getElementById("P").innerHTML = (year % 100 === 0) ? (year % 400 === 0): (year % 4 === 0); 
+	
---
+++ js202.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+function socialNumber() { 
+var year = document.getElementById("year").value;  
+document.getElementById("P").innerHTML = (year % 2) < 1;
+}
